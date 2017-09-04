48e700619fd316468b81357831ad0bde16c3c96c
Dominik Kurbiel <dominik.kurbiel@gmail.com>
2017-09-04 23:48:11 +0200

    Display temperature in three scales. Display icon showing the weather

--- app/js/script.js
+++ app/js/script.js
@@ -8,15 +8,18 @@ var weatherApp = {
 }
 
 function weatherRequest() {
-  var xhr = new XMLHttpRequest();
   var apiKey = 'forecast?id=524901&APPID=04921bec1844082c62487f04f17783e2';
   
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}/${apiKey}`)
+  var result = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}/${apiKey}`)
     .then((resp) => resp.json())
     .then((data) => {
-      console.log(data);
+    console.log(data);
+      const tempK = (data.main.temp).toFixed(1);
+      const tempC = (tempK - 273.15).toFixed(1);
+      const tempF = ((9/5) * tempC + 32).toFixed(1);
       document.getElementById('city__name').innerHTML = data.name;
-      document.getElementById('city__temp').innerHTML = data.main.temp;
+      document.getElementById('city__temp').innerHTML = `Fahrenheit temperature: ${tempF}, Celcius temperature ${tempC}, Kelvin temperature ${tempK}`;
+      document.getElementById('temp__icon').src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     })
     .catch((error) => {
       console.log(error);
