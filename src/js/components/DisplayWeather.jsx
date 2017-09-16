35d704e8d1ea785ebe470ca35726ace70cf29e65
Dominik Kurbiel <dominik.kurbiel@gmail.com>
2017-09-16 23:59:28 +0200

    PropTypes in DisplayWeather component + airbnb eslint

--- src/js/components/DisplayWeather.jsx
+++ src/js/components/DisplayWeather.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { shape, string, number, array } from 'prop-types';
 
-export default class DisplayWeather extends React.Component {
-  constructor() {
-    super();
-    
-  }
-  
-  
-  render() {
-    
-    var data = this.props.data;
-    
-    return(
-      <div className="weather__city-response">
-        <h1>City: {data.name}</h1>
-        <p>Temperature: {(data.main.temp - 273.15).toFixed(1)}</p>
-        <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}/>
-      </div>
-    );
-  }
-}
+const DisplayWeather = props => (
+  <div className="weather__city-response">
+    <h1>City: {props.data.name}</h1>
+    <p>Temperature: {(props.data.main.temp - 273.15).toFixed(1)}</p>
+    <img
+      src={`http://openweathermap.org/img/w/${props.data.weather[0].icon}.png`}
+      alt="weather-icon"
+    />
+  </div>
+);
+
+DisplayWeather.propTypes = {
+  data: shape({
+    name: string.isRequired,
+    main: shape({
+      temp: number.isRequired,
+    }),
+    weather: array.isRequired,
+    icon: string.isRequired,
+  }).isRequired,
+};
+
+export default DisplayWeather;
